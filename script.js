e80aac491fae890fbf09c4a275f32e05ea2cad76
RikelmyAnd <rikandrade0@gmail.com>
2025-08-25 21:26:08 -0300

    feat: Adiciona mais citações à lista

---
+++ script.js
@@ -0,0 +1,16 @@
+JavaScript
+const citacoes = [
+ "A imaginação é mais importante que o conhecimento. - Albert Einstein",
+ "A vida é o que acontece enquanto você está ocupado fazendo outros planos. - John Lennon",
+ "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo. - Winston Churchill"
+
+];
+
+const citacaoEl = document.getElementById("citacao");
+const botaoEl = document.getElementById("novaCitacaoBtn");
+
+botaoEl.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * citacoes.length);
+    citacaoEl.textContent = citacoes[randomIndex];
+   });
+// Lógica para gerar citação será adicionada depois.
